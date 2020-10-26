6ec77916c72dbc182f75e733ddc9b088c42b7b07
roshanj655 <roshanj655@gmail.com>
2020-10-26 16:50:35 +0530

    added date in live market

--- src/app/eventtype/live-events/live-events.component.ts
+++ src/app/eventtype/live-events/live-events.component.ts
@@ -24,7 +24,7 @@ export class LiveEventsComponent implements OnInit {
 
   ngOnInit(): void {
     this.betappService.getAllLiveMarkets().subscribe(markets => {
-      // console.log(markets);
+      //  console.log(markets);
       this.liveMarkets=markets;
     })
     
